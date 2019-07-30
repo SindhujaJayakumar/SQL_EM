5a3e234bc0fbb860eb86c25e00cf1629be801936
SindhujaJayakumar <51009958+SindhujaJayakumar@users.noreply.github.com>
2019-07-30 12:53:13 -0500

    Final changes done and files are uploaded

--- Employee.sql
+++ Employee.sql
@@ -1,84 +1,5 @@
-CREATE TABLE employees (
-    emp_no int NOT NULL,
-    birth_date date ,
-    first_name varchar(20)  ,
-    last_name varchar(20)  ,
-    gender varchar(10)  ,
-    hire_date date ,
-    CONSTRAINT pk_employees PRIMARY KEY (
-        emp_no
-     )
-);
 
-CREATE TABLE departments (
-    dept_no varchar NOT NULL,
-    dept_name varchar(20)  ,
-    CONSTRAINT pk_departments PRIMARY KEY (
-        dept_no
-     )
-);
-
-CREATE TABLE salaries (
-    emp_no int  ,
-    salary money   ,
-    from_date date  ,
-    to_date date,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-);
-
---PRIMARY KEY (item_id, so_id),
-   --FOREIGN KEY (so_id) REFERENCES so_headers (id)
-
-
-
-CREATE TABLE dept_manager (
-    dept_no varchar   ,
-    emp_no int  ,
-    from_date date  ,
-    to_date date  ,
-	PRIMARY KEY (dept_no,emp_no)
-);
-
-
-CREATE TABLE dept_emp (
-    emp_no int   ,
-	dept_no varchar  ,
-    from_date date  ,
-    to_date date ,
-	PRIMARY KEY (dept_no,emp_no)
-);
-
-CREATE TABLE titles (
-    emp_no int NOT NULL,
-    title varchar  ,
-    from_date date   ,
-    to_date date ,  
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-);
-
-SELECT * FROM employees;
-
-SELECT * FROM departments;
-
-SELECT * FROM salaries ;
-
-SELECT * FROM titles;
-
-SELECT * FROM dept_manager;
-
-SELECT * FROM dept_emp ;
-
---DROP TABLE employees;
-
---DROP TABLE departments;
-
---DROP TABLE  salaries ;
-
---DROP TABLE titles;
-
---DROP TABLE  dept_manager;
-
---DROP TABLE  dept_emp ;
+Link to Quickdatabasediagrams https://app.quickdatabasediagrams.com/#/d/0Mb0NQ
 
 --1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 SELECT emp_no, last_name, first_name, gender 
@@ -86,15 +7,12 @@ FROM employees;
 
 SELECT *
 FROM salaries;
-
 --join employees and salary tables
 SELECT
     e.emp_no, e.last_name, e.first_name, e.gender, s.salary
 FROM
     employees e
 INNER JOIN salaries s ON e.emp_no = s.emp_no ;
-
-
 
 --2. List employees who were hired in 1986.
 
@@ -153,7 +71,6 @@ SELECT
 FROM employees 
 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no;
-
 
 
 --5. List all employees whose first name is "Hercules" and last names begin with "B."
